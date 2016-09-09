8326a288f8032034e9e31babdd31903a67be81ea
tavisca-smurkar <smurkar@tavisca.com>
2016-09-09 09:29:57 +0530

    service host added successfully

---
+++ WCFServiceWithIIS/ServiceLibrary/EmployeeService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLibrary
+{
+    public class EmployeeService : IEmployeeService
+    {
+        static List<Employee> Employee;
+
+        public EmployeeService()
+        {
+            if (Employee == null)
+            {
+                Employee = new List<Employee>();
+                Employee.Add(new Employee { Id = "1", Name = "Salman", DateOfBirth = DateTime.Now, Gender = Gender.Male });
+                Employee.Add(new Employee { Id = "2", Name = "Shahrukh", DateOfBirth = DateTime.Now, Gender = Gender.Male });
+                Employee.Add(new Employee { Id = "3", Name = "Aamir", DateOfBirth = DateTime.Now, Gender = Gender.Male });
+            }
+
+        }
+
+
+        public Employee GetEmployee(string id)
+        {
+            return Employee.Find(x => x.Id.Equals(id));
+        }
+
+        public void SaveEmployee(Employee employee)
+        {
+            Employee.Add(employee);
+        }
+    }
+}
---
+++ WCFServiceWithIIS/ServiceLibrary/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+
+namespace ServiceLibrary
+{
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+
+        [OperationContract]
+        Employee GetEmployee(string id);
+
+        [OperationContract]
+        void SaveEmployee(Employee employee);
+    }
+}
